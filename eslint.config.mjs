594d55bcd8a815ce75c31c37c3cb2ac6e8a03434
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2025-06-27 18:29:16 +0800

    fix eslint rule errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,17 +5,14 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [{
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  "rules": {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-  }
-}
+export default [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  }),
 ];
-
-export default eslintConfig;
